8536d2d8d52046f6f6264cb0dc3758b226a248ee
k1o0 <k1o0@3tk.co>
2019-01-14 14:15:10 +0000

    Updated key protocols

--- Miles/advancedChoiceWorld.m
+++ Miles/advancedChoiceWorld.m
@@ -6,24 +6,29 @@ function advancedChoiceWorld(t, evts, p, vs, in, out, audio)
 % 2017-08    Added baited trials (thanks PZH)
 % 2017-09-26 Added manual reward key presses
 % 2017-10-26 p.wheelGain now in mm/deg units
+% 2018-03-15 Added time sampler function for delays
 
 %% parameters
 wheel = in.wheel.skipRepeats(); % skipRepeats means that this signal doesn't update if the new value is the same of the previous one (i.e. if the wheel doesn't move)
 rewardKey = p.rewardKey.at(evts.expStart); % get value of rewardKey at experiemnt start, otherwise it will take the same value each new trial
 rewardKeyPressed = in.keyboard.strcmp(rewardKey); % true each time the reward key is pressed
-nAudChannels = 2;
-p.audDevIdx; % Windows' audio device index (default is 1)
-audSampleRate = 44100; % Check PTB Snd('DefaultRate');
 contrastLeft = p.stimulusContrast(1);
 contrastRight = p.stimulusContrast(2);
 
 %% when to present stimuli & allow visual stim to move
-stimulusOn = evts.newTrial; % stimulus should come on at the start of a new trial
-interactiveOn = stimulusOn.delay(p.interactiveDelay); % the closed-loop period starts when the stimulus comes on, plus an 'interactive delay'
+% stimulus should come on after the wheel has been held still for the
+% duration of the preStimulusDelay.  The quiescence threshold is a tenth of
+% the rotary encoder resolution.
+preStimulusDelay = p.preStimulusDelay.map(@timeSampler).at(evts.newTrial); % at(evts.newTrial) fix for rig pre-delay 
+stimulusOn = sig.quiescenceWatch(preStimulusDelay, t, wheel, floor(p.encoderRes/100));
+interactiveDelay = p.interactiveDelay.map(@timeSampler);
+interactiveOn = stimulusOn.delay(interactiveDelay); % the closed-loop period starts when the stimulus comes on, plus an 'interactive delay'
 
+audioDevice = audio.Devices('default');
 onsetToneSamples = p.onsetToneAmplitude*...
-    mapn(p.onsetToneFrequency, 0.1, audSampleRate, 0.02, nAudChannels, @aud.pureTone); % aud.pureTone(freq, duration, samprate, "ramp duration", nAudChannels)
-audio.onsetTone = onsetToneSamples.at(interactiveOn); % At the time of 'interative on', send samples to audio device and log as 'onsetTone'
+    mapn(p.onsetToneFrequency, 0.1, audioDevice.DefaultSampleRate,...
+    0.02, audioDevice.NrOutputChannels, @aud.pureTone); % aud.pureTone(freq, duration, samprate, "ramp duration", nAudChannels)
+audio.default = onsetToneSamples.at(interactiveOn); % At the time of 'interative on', send samples to audio device and log as 'onsetTone'
 
 %% wheel position to stimulus displacement
 % Here we define the multiplication factor for changing the wheel signal
@@ -58,11 +63,11 @@ correctResponse = cond(contrastLeft > contrastRight, -1,... % contrast left
     (contrastLeft == contrastRight) & (rndDraw > 0), 1); % equal contrast (baited)
 feedback = correctResponse == response;
 % Only update the feedback signal at the time of the threshold being crossed
-feedback = feedback.at(threshold); 
+feedback = feedback.at(threshold).delay(0.1); 
 
 noiseBurstSamples = p.noiseBurstAmp*...
-    mapn(nAudChannels, p.noiseBurstDur*audSampleRate, @randn);
-audio.noiseBurst = noiseBurstSamples.at(feedback==0); % When the subject gives an incorrect response, send samples to audio device and log as 'noiseBurst'
+    mapn(audioDevice.NrOutputChannels, p.noiseBurstDur*audioDevice.DefaultSampleRate, @randn);
+audio.default = noiseBurstSamples.at(feedback==0); % When the subject gives an incorrect response, send samples to audio device and log as 'noiseBurst'
 
 reward = merge(rewardKeyPressed, feedback > 0);% only update when feedback changes to greater than 0, or reward key is pressed
 out.reward = p.rewardSize.at(reward); % output this signal to the reward controller
@@ -77,7 +82,7 @@ azimuth = cond(...
 
 % Test stim left
 leftStimulus = vis.grating(t, 'sinusoid', 'gaussian'); % create a Gabor grating
-leftStimulus.orientation = p.stimulusOrientation;
+leftStimulus.orientation = p.stimulusOrientation(1);
 leftStimulus.altitude = 0;
 leftStimulus.sigma = [9,9]; % in visual degrees
 leftStimulus.spatialFreq = p.spatialFrequency; % in cylces per degree
@@ -91,7 +96,7 @@ vs.leftStimulus = leftStimulus; % store stimulus in visual stimuli set and log a
 
 % Test stim right
 rightStimulus = vis.grating(t, 'sinusoid', 'gaussian');
-rightStimulus.orientation = p.stimulusOrientation;
+rightStimulus.orientation = p.stimulusOrientation(2);
 rightStimulus.altitude = 0;
 rightStimulus.sigma = [9,9];
 rightStimulus.spatialFreq = p.spatialFrequency;
@@ -110,11 +115,15 @@ nextCondition = feedback > 0 | p.repeatIncorrect == false;
 % we want to save these signals so we put them in events with appropriate
 % names:
 evts.stimulusOn = stimulusOn;
+evts.preStimulusDelay = preStimulusDelay;
 % save the contrasts as a difference between left and right
 evts.contrast = p.stimulusContrast.map(@diff); 
+evts.contrastLeft = contrastLeft;
+evts.contrastRight = contrastRight;
 evts.azimuth = azimuth;
 evts.response = response;
 evts.feedback = feedback;
+evts.interactiveOn = interactiveOn;
 % Accumulate reward signals and append microlitre units
 evts.totalReward = out.reward.scan(@plus, 0).map(fun.partial(@sprintf, '%.1fµl')); 
 
@@ -122,27 +131,64 @@ evts.totalReward = out.reward.scan(@plus, 0).map(fun.partial(@sprintf, '%.1fµl')
 % If the value of evts.endTrial is false, the current set of conditional
 % parameters are used for the next trial, if evts.endTrial updates to true, 
 % the next set of randowmly picked conditional parameters is used
-evts.endTrial = nextCondition.at(stimulusOff).delay(p.interTrialDelay); 
+evts.endTrial = nextCondition.at(stimulusOff).delay(p.interTrialDelay.map(@timeSampler)); 
 
 %% Parameter defaults
+% See timeSampler for full details on what values the *Delay paramters can
+% take.  Conditional perameters are defined as having ncols > 1, where each
+% column is a condition.  All conditional paramters must have the same
+% number of columns.
 try
-p.onsetToneFrequency = 8000;
-p.stimulusContrast = [1 0;0 1;0.5 0;0 0.5]'; % conditional parameters have ncols > 1
-p.repeatIncorrect = true;
+c = [1 0.5 0.25 0.12 0.06 0];
+%%% Contrast starting set
+% C = [1 0;0 1;0.5 0;0 0.5]';
+%%% Contrast discrimination set
+% c = combvec(c, c);
+% C = unique([c, flipud(c)]', 'rows')';
+%%% Contrast detection set
+C = [c, zeros(1, numel(c)-1); zeros(1, numel(c)-1), c];
+p.stimulusContrast = C;
+p.repeatIncorrect = abs(diff(C,1)) > 0.25 | all(C==0);
+p.onsetToneFrequency = 5000;
 p.interactiveDelay = 0.4;
-p.onsetToneAmplitude = 0.2;
+p.onsetToneAmplitude = 0.15;
 p.responseWindow = Inf;
 p.stimulusAzimuth = 90;
 p.noiseBurstAmp = 0.01;
 p.noiseBurstDur = 0.5;
 p.rewardSize = 3;
 p.rewardKey = 'r';
-p.stimulusOrientation = 0;
+p.stimulusOrientation = [0, 0]';
 p.spatialFrequency = 0.19; % Prusky & Douglas, 2004
 p.interTrialDelay = 0.5;
-p.wheelGain = 3;
-p.audDevIdx = 1;
+p.wheelGain = 5;
 p.encoderRes = 1024;
-catch
+p.preStimulusDelay = [0 0.1 0.09]';
+catch % ex
+%    disp(getReport(ex, 'extended', 'hyperlinks', 'on'))
+end
+
+%% Helper functions
+function duration = timeSampler(time)
+% TIMESAMPLER Sample a time from some distribution
+%  If time is a single value, duration is that value.  If time = [min max],
+%  then duration is sampled uniformally.  If time = [min, max, time const],
+%  then duration is sampled from a exponential distribution, giving a flat
+%  hazard rate.  If numel(time) > 3, duration is a randomly sampled value
+%  from time.
+%
+% See also exp.TimeSampler
+  if nargin == 0; duration = 0; return; end
+  switch length(time)
+    case 3 % A time sampled with a flat hazard function
+      duration = time(1) + exprnd(time(3));
+      duration = iff(duration > time(2), time(2), duration);
+    case 2 % A time sampled from a uniform distribution
+      duration = time(1) + (time(2) - time(1))*rand;
+    case 1 % A fixed time
+      duration = time(1);
+    otherwise % Pick on of the values
+      duration = randsample(time, 1);
+  end
 end
 end
