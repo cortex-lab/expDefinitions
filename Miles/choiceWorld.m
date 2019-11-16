18980330f0a610b1dfb3167d80120e2af8862abb
Miles Wells <k1o0@3tk.co>
2019-11-16 17:24:05 +0200

    Correct display of microlitres

--- Miles/choiceWorld.m
+++ Miles/choiceWorld.m
@@ -205,7 +205,8 @@ events.useContrasts = trialData.useContrasts;
 events.trialsToZeroContrast = trialData.trialsToZeroContrast;
 events.hitBuffer = trialData.hitBuffer;
 events.wheelGain = wheelGain;
-events.totalWater = outputs.reward.scan(@plus, 0).map(fun.partial(@sprintf, '%.1fµl'));
+volumeUnits = fun.partial(@sprintf, '%2$.1f%1$cl', char(956));
+events.totalWater = outputs.reward.scan(@plus, 0).map(volumeUnits);
 
 %% Defaults
 try 
